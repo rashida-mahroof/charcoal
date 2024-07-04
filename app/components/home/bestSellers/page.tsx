18eaf02bc9700ac597a81167d486ca8ac2aea387
rashidamahroof <rashidamahroofp@gmail.com>
2024-07-04 09:17:22 +0400

    h

--- app/components/home/bestSellers/page.tsx
+++ app/components/home/bestSellers/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 import React from 'react';
-import Image from 'next/image';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
 import ProdImage from '../../../assets/Charcoal-2.png';
-import CartButton from '../../product/cartButton/page';
-import WishListButton from '../../product/wishlistButton/page';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import ProductCard from '../../product/itemCard/page';
 
 const products = [
   { id: 1, name: 'Product 1', price: '$9.00 - $60.00', rating: 5, image: ProdImage },
@@ -19,35 +15,7 @@ const products = [
   { id: 6, name: 'Product 6', price: '$44.00', rating: 5, image: ProdImage },
 ];
 
-const ProductCard = ({ product }: any) => (
-  <div className="flex flex-col items-center bg-white px-2">
-    <div className="relative w-full aspect-square mb-4 group overflow-hidden">
-      <Image 
-        src={product.image} 
-        alt={product.name} 
-        layout="fill" 
-        objectFit="cover"
-        className="rounded-lg group-hover:scale-110 transition-transform duration-300"
-      />
-      {product.id === 3 && (
-        <span className="absolute top-2 left-2 bg-green-500 text-white text-xs font-bold px-2 py-1 rounded">
-          NEW
-        </span>
-      )}
-    </div>
-    <h3 className="text-lg font-semibold mb-1">{product.name}</h3>
-    <p className="text-gray-600 mb-2">{product.price}</p>
-    <div className="flex">
-      {[...Array(product.rating)].map((_, i) => (
-        <FontAwesomeIcon key={i} icon={faStar} className="text-yellow-400 w-4 h-4" />
-      ))}
-    </div>
-    <div className="flex flex-row justify-between items-center w-full mt-2">
-        <CartButton>Add To Cart</CartButton>
-        <WishListButton>Wish List</WishListButton>
-    </div>
-  </div>
-);
+
 
 const OurBestsellers = () => {
   const settings = {
